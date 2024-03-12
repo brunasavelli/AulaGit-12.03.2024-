b94f26b8bb39d648b74713841b12aecce4d6e6fd
Bruna Savelli <bnsavelli@gmail.com>
2024-03-12 08:53:51 -0300

    A temperatura foi alterada

--- temperatura.js
+++ temperatura.js
@@ -1,7 +1,7 @@
-let a = 29.5;
+let a = 40;
 
-//realizar transformação
-let transformação = a * 9/5 + 32;
+//realizar formula
+let formula = a * 9/5 + 32;
 
 //Exibir mensagem
 console.log("Bom dia! A temperatura de hoje é de " + a.toFixed(2) + "°C" + ". Hoje a temperatura em Fahrenheit é de " + transformação.toFixed(2) + "°F")
