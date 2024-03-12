04ab24b1cf9c342c885d16ddaeb209c8d2f748b1
Bruna Savelli <bnsavelli@gmail.com>
2024-03-12 09:17:04 -0300

    Houve mudanças no código

---
+++ exercicioContador.js
@@ -0,0 +1,36 @@
+let a = 0;
+
+let b = 10;
+
+let inicio;
+
+let fim;
+
+let par;
+
+let soma = 0;
+
+
+if (a < b) {
+    inicio = a;
+    fim = b;
+} else {
+    inicio = b;
+    fim = a;
+}
+
+while (inicio <= fim) {
+    if (inicio % 2 == 0) {
+        console.log(inicio)
+        soma = inicio + soma;
+    }
+
+    inicio++
+
+    //exibir soma
+
+}
+
+console.log(soma)
+
+
