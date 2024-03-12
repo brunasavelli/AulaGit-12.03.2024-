c889d599de0724b23c6c215b459957ec56e59099
Bruna Savelli <bnsavelli@gmail.com>
2024-03-12 08:59:26 -0300

    A mensagem foi alterada

--- temperatura.js
+++ temperatura.js
@@ -4,7 +4,7 @@ let a = 40;
 let formula = a * 9/5 + 32;
 
 //Exibir mensagem
-console.log("Bom dia! A temperatura de hoje é de " + a.toFixed(2) + "°C" + ". Hoje a temperatura em Fahrenheit é de " + transformação.toFixed(2) + "°F")
+console.log("Seja bem-vindo! A temperatura de hoje é de " + a.toFixed(2) + "°C" + ". Hoje a temperatura em Fahrenheit é de " + transformação.toFixed(2) + "°F")
 
 
 
